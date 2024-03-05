a163f303fbb538c2a5c81f713eb2249f12c31b5c
hyundesk <hyundesk1006@gmail.com>
2024-03-05 14:56:18 +0900

    Footer 업테이트

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./view/Main";
 import Header from "./Components/Header";
@@ -18,11 +18,11 @@ import CompleteOrder from "./Components/Shop/CompleteOrder"; // 이기현_주문
 import MyOrderList from "./Components/Shop/MyOrderList"; // 이기현_주문내역 조회 컴포넌트
 
 //-------------------------
-import Higherlist from './view/Higherlist';
+import Higherlist from "./view/Higherlist";
 import Latestlist from "./view/Latestlist";
 import Lowerlist from "./view/Lowerlist";
 import Product from "./view/Product";
-import Productregister from './view/Productregister'
+import Productregister from "./view/Productregister";
 import Reviewwriter from "./view/Reviewwirter";
 
 function App() {
@@ -30,58 +30,48 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        <Routes>
-          {/* Main Page */}
-          <Route exact path="/" element={<Main />} />
-          {/* 이기현 */}
-          <Route exact path="/cart" element={<Cart />} />
-          {/* "/" 로컬 장바구니 페이지 라우팅 */}
-          <Route exact path="/ordersheet" element={<Ordersheet />} />
-          {/* "/" 주문서 작성 페이지 라우팅 */}
-          <Route exact path="/completeOrder" element={<CompleteOrder />} />
-          {/* "/" 주문 완료 페이지 라우팅 */}
-          <Route exact path="/myOrderList" element={<MyOrderList />} />
-          {/* "/" 주문 내역 조회 페이지 라우팅 */}
-          {/* 이주호 */}
-          {/* 김민호 */}
-          <Route path="/Login" element={<LoginPage />}></Route>
-          <Route path="/Modify" element={<Modify/>}></Route>
-          <Route path="/Regester" element={<Regester />}></Route>
-          <Route path="/Regester/personal" element={<RegesterPersonal/>}></Route>
-          <Route path="/Regester/corporate" element={<RegesterCorporate/>}></Route>
-          <Route path="/Regester/group" element={<RegesterGroup/>}></Route>
+        <div className="App_main_height">
+          <Routes>
+            {/* Main Page */}
+            <Route exact path="/" element={<Main />} />
+            {/* 이기현 */}
+            <Route exact path="/cart" element={<Cart />} />
+            {/* "/" 로컬 장바구니 페이지 라우팅 */}
+            <Route exact path="/ordersheet" element={<Ordersheet />} />
+            {/* "/" 주문서 작성 페이지 라우팅 */}
+            <Route exact path="/completeOrder" element={<CompleteOrder />} />
+            {/* "/" 주문 완료 페이지 라우팅 */}
+            <Route exact path="/myOrderList" element={<MyOrderList />} />
+            {/* "/" 주문 내역 조회 페이지 라우팅 */}
+            {/* 이주호 */}
+            {/* 김민호 */}
+            <Route path="/Login" element={<LoginPage />}></Route>
+            <Route path="/Modify" element={<Modify />}></Route>
+            <Route path="/Regester" element={<Regester />}></Route>
+            <Route
+              path="/Regester/personal"
+              element={<RegesterPersonal />}
+            ></Route>
+            <Route
+              path="/Regester/corporate"
+              element={<RegesterCorporate />}
+            ></Route>
+            <Route path="/Regester/group" element={<RegesterGroup />}></Route>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-          {/* 전윤호 */}
-          {/* <Route path="/shop" element={<Shop />} /> */}
-          <Route path="/shop/:categoryid/1/:page" element={<Latestlist />} />
-          <Route path="/shop/:categoryid/2/:page" element={<Higherlist />} />
-          <Route path="/shop/:categoryid/3/:page" element={<Lowerlist />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/register" element={<Productregister />} />
-          <Route path="/review/:userid/:orderid/:prodid" element={<Reviewwriter />} />
-
-
-
-
-
-
-
-
-        </Routes>
-        {/* <Footer /> */}
+            {/* 전윤호 */}
+            {/* <Route path="/shop" element={<Shop />} /> */}
+            <Route path="/shop/:categoryid/1/:page" element={<Latestlist />} />
+            <Route path="/shop/:categoryid/2/:page" element={<Higherlist />} />
+            <Route path="/shop/:categoryid/3/:page" element={<Lowerlist />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/register" element={<Productregister />} />
+            <Route
+              path="/review/:userid/:orderid/:prodid"
+              element={<Reviewwriter />}
+            />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
